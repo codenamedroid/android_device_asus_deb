a9f25bf76804fe992562655c8aece4c86f27909d
Android Dev <codenamegit@gmail.com>
2013-12-29 12:26:53 -0800

    initial changes for cna

--- aosp_deb.mk
+++ aosp_deb.mk
@@ -20,12 +20,17 @@
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/deb/device.mk)
 
+# Get the long list of APNs
+$(call inherit-product, vendor/cna/configs/apns.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := aosp_deb
 PRODUCT_DEVICE := deb
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Deb
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := ASUS
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT=google/razorg/deb:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="razorg-user 4.4.2 KOT49H 937116 release-keys"
